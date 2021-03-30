32145ff12a3b05a23b44521b35622363a2e1759b
Legend1998 <sumankuma.1997@gmail.com>
2021-03-30 11:15:57 +0530

    added stateprovider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <div className=" h-screen flex items-center justify-center border">
-        <h1 className="text-green-700 font-mono text-7xl">yooooooooooooo</h1>
+        <h1 className="text-green-700 font-mono lg:text-7xl md:text-5xl sm:text-3xl">
+          yooooooooooooo
+        </h1>
       </div>
     </>
   );
